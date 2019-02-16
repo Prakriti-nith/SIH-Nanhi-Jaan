384897d0bd2a33c740977a8aaf039f7defb715f3
Prakriti-nith <prakriti.gupta10@gmail.com>
2019-02-16 21:25:59 +0530

    Make Cardview dynamic

--- app/src/main/java/com/example/nanhijaan/MainActivity.java
+++ app/src/main/java/com/example/nanhijaan/MainActivity.java
@@ -1,31 +1,104 @@
 package com.example.nanhijaan;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Context mContext;
     Toolbar toolbar;
     FloatingActionButton fab;
-    CardView disease_cp_cv, disease_au_cv, disease_ds_cv, disease_cv;
+    RelativeLayout disease_rl;
+    int num_diseases;
+    String disease_names;
+    CardView disease_cv[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = getApplicationContext();
 
         init();
         setListener();
 
         setSupportActionBar(toolbar);
+    }
+
+    private void init() {
+        toolbar = findViewById(R.id.toolbar);
+        fab = findViewById(R.id.fab);
+        disease_rl = findViewById(R.id.cards_rl);
+
+        num_diseases = 10; // Fetch from API disease number, disease names, and images;
+        placeCards(num_diseases);
+    }
+
+    private void placeCards(int num_diseases) {
+        RelativeLayout.LayoutParams relParams[] = new RelativeLayout.LayoutParams[num_diseases];
+        disease_cv = new CardView[num_diseases];
+
+        for(int i=0; i<num_diseases; i++) {
+            disease_cv[i] = new CardView(mContext);
+            disease_cv[i].setCardBackgroundColor(Color.parseColor("#FDFDFD"));
+            disease_cv[i].setCardElevation(4);
+            disease_cv[i].setId(i+1);
+            disease_cv[i].setRadius(10);
+
+
+            relParams[i] = new RelativeLayout.LayoutParams(500, 500);
+            relParams[i].setMargins(10, 10, 10, 10);
+            if(i%2 == 1) {
+                relParams[i].addRule(RelativeLayout.RIGHT_OF, disease_cv[i-1].getId());
+                if(i != 1) {
+                    relParams[i].addRule(RelativeLayout.BELOW, disease_cv[i-2].getId());
+                }
+            }
+            else if(i%2 == 0 && i != 0) {
+                relParams[i].addRule(RelativeLayout.BELOW, disease_cv[i-2].getId());
+            }
+
+
+            //            Put ImageView on CardView
+            ImageView iv = new ImageView(mContext);
+            iv.setId(i + 10000);
+            iv.setImageResource(R.mipmap.ic_launcher);  // TODO: From API
+            RelativeLayout.LayoutParams ivparams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            ivparams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+            ivparams.setMargins(150,150,100,100);
+            iv.setLayoutParams(ivparams);
+
+
+            //            Put the TextView in CardView
+            TextView tv = new TextView(mContext);
+            RelativeLayout.LayoutParams tvparams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            tvparams.addRule(RelativeLayout.BELOW, iv.getId());
+            tv.setText("CardView" + i); // TODO: From API
+            tvparams.setMargins(150,350,100,100); // TODO: NEED TO CHANGE... SHOULD BE BELOW IMAGEVIEW
+            //tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 30);
+            tv.setTextColor(Color.BLACK);
+            tv.setLayoutParams(tvparams);
+
+
+            // Finally, add the CardView in root layout
+            disease_cv[i].addView(iv, ivparams);
+            disease_cv[i].addView(tv, tvparams);
+            disease_rl.addView(disease_cv[i], relParams[i]);
+        }
     }
 
     private void setListener() {
@@ -36,51 +109,17 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        disease_cp_cv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String disease_name = "Cerebral Palsy";
-                Intent i = new Intent(MainActivity.this, DiseaseDetailsActivity.class);
-                i.putExtra("disease",disease_name);
-                startActivity(i);
-            }
-        });
-        disease_au_cv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String disease_name = "Cerebral Palsy";
-                Intent i = new Intent(MainActivity.this, DiseaseDetailsActivity.class);
-                i.putExtra("disease",disease_name);
-                startActivity(i);
-            }
-        });
-        disease_ds_cv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String disease_name = "Cerebral Palsy";
-                Intent i = new Intent(MainActivity.this, DiseaseDetailsActivity.class);
-                i.putExtra("disease",disease_name);
-                startActivity(i);
-            }
-        });
-        disease_cv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String disease_name = "Cerebral Palsy";
-                Intent i = new Intent(MainActivity.this, DiseaseDetailsActivity.class);
-                i.putExtra("disease",disease_name);
-                startActivity(i);
-            }
-        });
-    }
-
-    private void init() {
-        toolbar = findViewById(R.id.toolbar);
-        fab = findViewById(R.id.fab);
-        disease_cp_cv = findViewById(R.id.diseasecv1);
-        disease_au_cv = findViewById(R.id.diseasecv2);
-        disease_ds_cv = findViewById(R.id.diseasecv3);
-        disease_cv = findViewById(R.id.diseasecv4);
+        for(int i=0; i<num_diseases; i++) {
+            disease_cv[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final String disease_name = "Cerebral Palsy"; // From API
+                    Intent i = new Intent(MainActivity.this, DiseaseDetailsActivity.class);
+                    i.putExtra("disease",disease_name);
+                    startActivity(i);
+                }
+            });
+        }
     }
 
     @Override
