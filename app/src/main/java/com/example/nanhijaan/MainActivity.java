0611d8d0fb7b7c5b4f126a2bd23f2dd89b267a50
Prakriti-nith <prakriti.gupta10@gmail.com>
2019-02-18 22:26:27 +0530

    record audio

--- app/src/main/java/com/example/nanhijaan/MainActivity.java
+++ app/src/main/java/com/example/nanhijaan/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.nanhijaan;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,23 +38,42 @@ public class MainActivity extends AppCompatActivity {
     int num_diseases;
     String disease_names;
     CardView disease_cv[];
+    ImageButton search_ib;
+
+    private static final String AUDIO_RECORDER_FILE_EXT_3GP = ".3gp";
+    private static final String AUDIO_RECORDER_FILE_EXT_MP4 = ".mp3";
+    private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
+    public static final int RECORD_AUDIO = 0;
+    private static final int REQUEST_CODE = 1;
+
+
+    private MediaRecorder recorder = null;
+    private int currentFormat = 0;
+    private int output_formats[] = { MediaRecorder.OutputFormat.MPEG_4, MediaRecorder.OutputFormat.THREE_GPP };
+    private String file_exts[] = { AUDIO_RECORDER_FILE_EXT_MP4, AUDIO_RECORDER_FILE_EXT_3GP };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContext = getApplicationContext();
-
+        if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+        }
         init();
         setListener();
 
         setSupportActionBar(toolbar);
+
+
     }
 
     private void init() {
         toolbar = findViewById(R.id.toolbar);
         fab = findViewById(R.id.fab);
         disease_rl = findViewById(R.id.cards_rl);
+        search_ib = findViewById(R.id.searchib);
 
         num_diseases = 10; // Fetch from API disease number, disease names, and images;
         placeCards(num_diseases);
@@ -101,6 +132,62 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private String getFilename(){
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath,AUDIO_RECORDER_FOLDER);
+
+        if(!file.exists()){
+            file.mkdirs();
+        }
+
+        return (file.getAbsolutePath() + "/nanhijansound" + file_exts[currentFormat]);
+    }
+
+
+    private MediaRecorder.OnErrorListener errorListener = new MediaRecorder.OnErrorListener() {
+        @Override
+        public void onError(MediaRecorder mr, int what, int extra) {
+            AppLog.logString("Error: " + what + ", " + extra);
+        }
+    };
+
+    private MediaRecorder.OnInfoListener infoListener = new MediaRecorder.OnInfoListener() {
+        @Override
+        public void onInfo(MediaRecorder mr, int what, int extra) {
+            AppLog.logString("Warning: " + what + ", " + extra);
+        }
+    };
+
+    private void startRecording(){
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(output_formats[currentFormat]);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(getFilename());
+        recorder.setOnErrorListener(errorListener);
+        recorder.setOnInfoListener(infoListener);
+
+        try {
+            recorder.prepare();
+            recorder.start();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void stopRecording(){
+        if(null != recorder){
+            recorder.stop();
+            recorder.reset();
+            recorder.release();
+
+            recorder = null;
+        }
+    }
+
     private void setListener() {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,6 +196,47 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        search_ib.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    float x = (float) 2;
+                    float y = (float) 2;
+
+                    search_ib.setScaleX(x);
+                    search_ib.setScaleY(y);
+                    //search_ib.setBackgroundResource(R.drawable.blue_220);
+
+                    AppLog.logString("Start Recording");
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, RECORD_AUDIO);
+
+                    } else{
+                        startRecording();
+                    }
+                    return true;
+                }
+
+                else if(event.getAction() == MotionEvent.ACTION_UP)
+                {
+                    float x = 1;
+                    float y = 1;
+
+                    search_ib.setScaleX(x);
+                    search_ib.setScaleY(y);
+
+                    AppLog.logString("stop Recording");
+                    stopRecording();
+                }
+                return false;
+            }
+        });
+
+
         for(int i=0; i<num_diseases; i++) {
             disease_cv[i].setOnClickListener(new View.OnClickListener() {
                 @Override
