2ee57d24068363d6bd8c265282e112a238e2e7c0
sunidhi chaudhary <sunidhi.nith@gmail.com>
2019-03-02 21:51:22 +0530

    slider&languages

--- app/src/main/java/com/example/nanhijaan/UrlHelper.java
+++ app/src/main/java/com/example/nanhijaan/UrlHelper.java
@@ -5,8 +5,8 @@ public class UrlHelper {
     language = {hindi, english, punjabi, telugu}
     id = */
 
-    public static final String SIHAPI_URL = "http://nithsih.herokuapp.com/sihapi/getDataLang/?publish_status=1&language=";
-    public static final String SIHAPI_DISEASE_URL = "http://nithsih.herokuapp.com/sihapi/getResource/?publish_status=1&id=";
+    public static final String SIHAPI_URL = "http://sihnith.herokuapp.com/sihapi/getDataLang/?publish_status=1&language=";
+    public static final String SIHAPI_DISEASE_URL = "http://sihnith.herokuapp.com/sihapi/getResource/?publish_status=1&id=";
     public static final String SIHAPI_POST_PARENTS_URL = "http://sihnith.herokuapp.com/sihapi/feedback/";
 
 }
