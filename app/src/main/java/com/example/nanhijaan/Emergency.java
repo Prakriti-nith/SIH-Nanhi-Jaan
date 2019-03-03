97582bbef47892d90b1de197e3ec05f62ab24493
sunidhi chaudhary <sunidhi.nith@gmail.com>
2019-03-03 12:42:32 +0530

    doctor1

--- app/src/main/java/com/example/nanhijaan/Emergency.java
+++ app/src/main/java/com/example/nanhijaan/Emergency.java
@@ -32,7 +32,8 @@ public class Emergency extends AppCompatActivity {
         doc1_cv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+ "7018352301"));
+               // Intent intent = new Intent(getApplicationContext(),POP_UP.class);
+               // startActivity(intent);
             }
         });
         doc2_cv.setOnClickListener(new View.OnClickListener() {
@@ -44,8 +45,8 @@ public class Emergency extends AppCompatActivity {
         doc3_cv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+ "7018352301"));
-                startActivity(intent);
+                // Intent intent = new Intent(getApplicationContext(),POP_UP.class);
+                // startActivity(intent);
             }
         });
         doc4_cv.setOnClickListener(new View.OnClickListener() {
@@ -57,13 +58,15 @@ public class Emergency extends AppCompatActivity {
         doc5_cv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+ "7018352301"));
+                // Intent intent = new Intent(getApplicationContext(),POP_UP.class);
+                // startActivity(intent);
             }
         });
         doc6_cv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+ "7018352301"));
+                // Intent intent = new Intent(getApplicationContext(),POP_UP.class);
+                // startActivity(intent);
             }
         });
     }
